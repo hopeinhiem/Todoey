11831c564c2bd57e87b8a06941a680493ea237e6
Andrew Greene <agreene@andrews-macbook-pro.local>
2018-10-17 19:41:50 -0400

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Andrew Greene on 2018-10-16.
+//  Copyright © 2018 Andrew Greene. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
