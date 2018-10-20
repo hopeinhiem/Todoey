2e9f36e6cd2a9e0984192f21f61a309b11b24eff
Andrew Greene <agreene@andrews-macbook-pro.local>
2018-10-20 19:20:16 -0400

    CategoryViewController adder, added persistent storage using coreData

---
+++ Todoey/Controllers/CatagoryViewController.swift
@@ -0,0 +1,37 @@
+//
+//  CatagoryViewController.swift
+//  Todoey
+//
+//  Created by Andrew Greene on 2018-10-20.
+//  Copyright © 2018 Andrew Greene. All rights reserved.
+//
+
+import UIKit
+import CoreData
+class CatagoryViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+    //MARK: - TableView Datasource Methods
+    
+    //MARK: - Data Manipulation Methods save data load data
+    
+    //Mark: - Add New Categories
+
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+    }
+    
+    //MARK: - TableView Datasource Methods
+    
+    //MARK: - Table Delegate Methods
+    
+    //MARK: - Data Manipulation Methods
+    
+    
+    
+}
